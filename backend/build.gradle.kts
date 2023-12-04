91d2b949b06bf585e0389731e27c4e4c0762a796
Roman Kolesnikov <roman.kolesnikov1@hs-augsburg.de>
2023-12-04 17:26:32 +0100

    create kotlin

---
+++ backend/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
+}
